0213ed18c045aed6c4860cf4457ce2e81bdb6ffe
Laura Rubio <laurarubio11@hotmail.com>
2021-02-17 18:26:23 -0500

    login

--- front-pdg/src/components/Index/Login.js
+++ front-pdg/src/components/Index/Login.js
@@ -4,6 +4,7 @@ import TextField from '@material-ui/core/TextField';
 import { Grid } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
 import Autocomplete from '@material-ui/lab/Autocomplete';
+import Button from '@material-ui/core/Button';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -26,19 +27,35 @@ export default function Login() {
                         <TextField id="standard-basic" label="Correo Electrónico/Usuario" fullWidth />
 
                     </Grid>
-                    <br></br>
+                    <Grid item xs={12} md={12}>
+                        <br></br>
+
+                    </Grid>
+
                     <Grid item xs={12}>
                         <TextField id="standard-basic" label="Contraseña" fullWidth />
                     </Grid>
+                    <Grid item xs={12} md={12}>
+                        <br></br>
+
+                    </Grid>
                     <Grid item xs={12}>
                         <Autocomplete
+                            fullWidth
                             id="combo-box-demo"
-                            //options={top100Films}
-                            //getOptionLabel={(option) => option.title}
-                            style={{ width: 300 }}
-                            renderInput={(params) => <TextField {...params} label="Combo box" variant="outlined" />}
+                        //options={top100Films}
+                        //getOptionLabel={(option) => option.title}
+                        // style={{ width: 300 }}
+                        renderInput={(params) => <TextField {...params} label="Institución" variant="outlined" />}
                         />
+                        <Grid item xs={12} md={12}>
+                            <br></br>
+
+                        </Grid>
+                        <Button fullWidth variant="contained" color="primary">Iniciar sesión</Button>
+
                     </Grid>
+
 
                 </form>
 
