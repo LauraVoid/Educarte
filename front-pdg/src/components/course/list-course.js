039582a64f4c49d9a5ae3b7ec8c9baf1aa730458
Laura Rubio <laurarubio11@hotmail.com>
2021-03-02 16:33:17 -0500

    update list course view

--- front-pdg/src/components/course/list-course.js
+++ front-pdg/src/components/course/list-course.js
@@ -1,6 +1,5 @@
-import { Grid, IconButton } from '@material-ui/core'
-import React,{ useState, useEffect } from 'react'
-import { makeStyles } from '@material-ui/core/styles';
+import { Grid } from '@material-ui/core'
+import React, { Component } from 'react'
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -9,118 +8,87 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
-import EyeButton from '@material-ui/icons/Visibility';
-import DeleteIcon from '@material-ui/icons/Delete';
-import EditIcon from '@material-ui/icons/Create';
-import axios from '../../utils/axios'
-import './style/create-course.css'
+import Course from './course'
+import axios from "axios";
+import './style/create-course.css';
 
-const useStyles = makeStyles({
-    root: {
-        minWidth: 275,
-    },
-    bullet: {
-        display: 'inline-block',
-        margin: '0 2px',
-        transform: 'scale(0.8)',
-    },
-    title: {
-        fontSize: 24,
-    },
-    pos: {
-        marginBottom: 12,
-    },
-    table: {
-        minWidth: 650,
-    },
-});
+export default class ListCourse extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            coursesView: false
+            
+        };
+    }
+    renderCourses = () => {
+        if(!this.state.coursesView){
+            axios.get('http://localhost:8000/course/')
+            .then((res) => {
+                if (res.status === 200) {
+                    let courses = res.data
+                    let itemCourse = courses.map(course => <Course key={course.id} course={course}></Course>)
+                    console.log(itemCourse)
+                    this.setState({
+                        coursesView:true
+                    })
+                    return itemCourse;
+
+                }
+                else console.log(res.status);
+            })
+            .catch((err) => console.log(err))
 
 
-export default function ListCourse() {
-    const classes = useStyles();
-    const  [courses, setCourses]= useState([]);
+        }
+     
+    }
+    render() {
+            return (
+                <div>
+                    <div style={{ padding: 20 }}>
+                        <h1>Gestionar cursos</h1>
 
-    // useEffect(() => {
+                        <div style={{ padding: 40, margin: 50 }}>
+                            <Grid container spacing={5} className="Grid-main-purple">
+                                <Grid item xs={12}>
+                                    <Button variant="contained" color="primary" href="/createcourse">
+                                        Agregar +
+                            </Button>
+
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TableContainer component={Paper}>
+                                        <Table className="table" aria-label="simple table">
+                                            <TableHead>
+                                                <TableRow>
+                                                    <TableCell >Id</TableCell>
+                                                    <TableCell >Curso</TableCell>
+                                                    <TableCell align="right">Ver estudiantes</TableCell>
+                                                    <TableCell align="right">Ver profesor(es)</TableCell>
+                                                    <TableCell align="right"></TableCell>
+                                                </TableRow>
+                                            </TableHead>
+                                            <TableBody>
+                                                {this.renderCourses()}
+                                            </TableBody>
+                                        </Table>
+                                    </TableContainer>
+                                </Grid>
+
+
+
+                            </Grid>
+
+                        </div >
+
+
+
+
+                    </div>
+                </div>
+            )
         
-    //     axios.get('course/')
-    //     .then((res)=>{
-    //         if(res.status === 200) setCourses(res.data)
-    //         else console.log(res.status);
-    //     })
-    //     .catch((err)=> console.log(err))
-        
-    // }, [courses])
 
-    return (
-        <div style={{ padding: 20 }}>
-            <h1>Gestionar cursos</h1>
-
-            <div style={{ padding: 40, margin:50 }}>
-                <Grid container spacing={5} className="Grid-main-purple">
-                    <Grid item xs={12}>
-                        <Button variant="contained" color="primary" href="/createcourse">
-                            Agregar +
-                        </Button>
-
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TableContainer component={Paper}>
-                            <Table className={classes.table} aria-label="simple table">
-                                <TableHead>
-                                    <TableRow>
-                                    <TableCell >Id</TableCell>
-                                        <TableCell >Curso</TableCell>                                       
-                                        <TableCell align="right">Ver estudiantes</TableCell>
-                                        <TableCell align="right">Ver profesor(es)</TableCell>
-                                        <TableCell align="right"></TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {courses.map((row) => (
-                                        <TableRow key={row.id}>
-                                            <TableCell >{row.id}</TableCell>
-                                            <TableCell component="th" scope="row">
-                                                {row.name}
-                                            </TableCell>                                            
-                                            <TableCell align="right">
-                                                <IconButton>
-                                                    <EyeButton />
-                                                </IconButton>
-                                            </TableCell>
-                                            <TableCell align="right">
-                                                <IconButton>
-                                                    <EyeButton />
-                                                </IconButton>
-                                            </TableCell>
-                                            <TableCell align="right">
-                                                
-                                            <IconButton>
-                                                    <EditIcon />
-                                                </IconButton>
-                                                <IconButton>
-                                                    <DeleteIcon />
-                                                </IconButton>
-                                                
-                                            </TableCell>
-
-
-                                        </TableRow>
-                                    ))}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                    </Grid>
-
-
-
-                </Grid>
-
-            </div >
-
-
-
-
-        </div>
-    )
-
+    }
 }
--- front-pdg/src/utils/axios.js
+++ front-pdg/src/utils/axios.js
@@ -1,15 +1,6 @@
 import axios from "axios";
 export default axios.create({
 
-    baseURL:'http://localhost:8000/',   
-
-    headers: {
-        "X-Requested-With": "XMLHttpRequest",
-        Authorization: {
-            toString() {
-                return `Bearer ${localStorage.getItem("access_token")}`;
-            },
-        },
-    },
+    baseURL:'http://localhost:8000/',     
   
 });
