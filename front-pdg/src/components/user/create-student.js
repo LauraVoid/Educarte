17832b31b473752b85327d320b5ea7c14f20dc8d
DouglasPapu <1005828986@u.icesi.edu.co>
2021-03-16 10:05:38 -0500

    images updated on name

--- front-pdg/src/components/user/create-student.js
+++ front-pdg/src/components/user/create-student.js
@@ -1,8 +1,117 @@
-import { Grid } from "@material-ui/core/MenuItem";
-export default createStudent = () => {
+import React from "react";
+import { makeStyles } from "@material-ui/styles";
+import { connect } from "react-redux";
+import { Grid, TextField, Typography, Button } from "@material-ui/core/";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "25ch",
+    },
+  },
+  gridContainer: {
+    backgroundColor: "white",
+    borderRadius: "2em",
+  },
+  componentsItems: {
+    margin: "2%",
+  },
+  buttonSave: {
+    marginLeft: "45%",
+    marginBottom: "2%",
+  },
+}));
+
+const CreateStudent = () => {
+  const classes = useStyles();
   return (
-    <Grid container>
-      <Grid item></Grid>
-    </Grid>
+    <div>
+      <Grid container className={classes.gridContainer}>
+        <Grid item sm={6}>
+          <form className={classes.root}>
+            <Typography
+              className={classes.componentsItems}
+              variant="h6"
+              color="initial"
+            >
+              Datos del estudiante:
+            </Typography>
+            <TextField
+              className={classes.componentsItems}
+              id="standard-basic"
+              label="Nombres"
+            />
+            <TextField
+              className={classes.componentsItems}
+              id="standard-basic"
+              label="Apellidos"
+            />
+            <TextField
+              className={classes.componentsItems}
+              id="standard-basic"
+              label="Fecha de nacimiento"
+            />
+            <TextField
+              className={classes.componentsItems}
+              id="standard-basic"
+              label="Registro de nacimiento"
+            />
+          </form>
+        </Grid>
+        <Grid item sm={6}>
+          <form className={classes.root}>
+            <Typography
+              className={classes.componentsItems}
+              variant="h6"
+              color="initial"
+            >
+              Datos del acudiente:
+            </Typography>
+            <TextField
+              className={classes.componentsItems}
+              id="standard-basic"
+              label="Nombres"
+            />
+            <TextField
+              className={classes.componentsItems}
+              id="standard-basic"
+              label="Apellidos"
+            />
+            <TextField
+              className={classes.componentsItems}
+              id="standard-basic"
+              label="Número de identificación"
+            />
+            <TextField
+              className={classes.componentsItems}
+              id="standard-basic"
+              label="Correo electrónico"
+            />
+          </form>
+        </Grid>
+        <Grid container>
+          <Grid item xs={12}>
+            <Button
+              className={classes.buttonSave}
+              variant="contained"
+              color="primary"
+              size="large"
+            >
+              Guardar
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
   );
 };
+
+const mapStateToProps = (state) => ({
+  // instid: state.auth.instId,
+});
+
+CreateStudent.propTypes = {
+  // instid: PropTypes.any,
+};
+export default connect(mapStateToProps)(CreateStudent);
--- front-pdg/src/pages/user/add-student.js
+++ front-pdg/src/pages/user/add-student.js
@@ -1,21 +1,54 @@
 import React from "react";
-import Grid from "@material-ui/core/Grid";
+import { Grid, Box } from "@material-ui/core/";
 //import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/styles";
 import { connect } from "react-redux";
+import bgd from "../../img/backgrounds/B6.png";
+import FormStudent from "../../components/user/create-student";
 
+// CSS OF THIS TEMPLATE
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+  },
+  divContainer: {
+    backgroundImage: `url(${bgd})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+  },
+  boxContainer: {
+    backgroundColor: "#2196f3",
+    marginLeft: "5%",
+    marginRight: "5%",
+    marginBottom: "5%",
+    borderRadius: "2em",
+    padding: "2%",
+  },
+  title: {
+    marginLeft: "3%",
   },
 }));
 
 const CreateStudent = () => {
   const classes = useStyles();
   return (
-    <Grid container className={classes.root} spacing={1}>
-      <Grid item>HOLA</Grid>
-    </Grid>
+    <div className={classes.divContainer}>
+      <Grid container>
+        <Grid item xs={12}>
+          <h1 className={classes.title}>Agregar un estudiante</h1>
+        </Grid>
+      </Grid>
+      <Grid container className={classes.root} justify="center">
+        <Box
+          className={classes.boxContainer}
+          color="text.primary"
+          justifyContent="center"
+        >
+          <FormStudent></FormStudent>
+        </Box>
+      </Grid>
+    </div>
   );
 };
 const mapStateToProps = (state) => ({
